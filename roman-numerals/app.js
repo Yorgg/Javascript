ffea0346c2d6314d70eaf4eadbd6c9a483f1cdad
Jon <jyorg@outlook.com>
2016-03-16 09:08:48 +0800

    Roman numerals - fix issues , add big numbers

--- roman-numerals/app.js
+++ roman-numerals/app.js
@@ -1,25 +1,20 @@
+//
+//
 $( document ).ready(function() {  
-  /* Set submitted to false */
-  var submitted = false;
 
   $("#submit").on( "click", function() {
-
-  	/* set it to true, to avoid keyup from being called if the user 
-  	   hits enter on the submit button */
-  	submitted = true;
-  	var input = $('input').val();
-  	var conversion = R$(input);
-    $("ul").prepend("<li>" + conversion + '<number>'+input+'</number>' +"</li>");
+  	var input = $('input').val(), 
+  	    roman = R$(input);
+    $("ul").prepend('<li>' + roman +'<num>' + input + '</num>' + '</li>');
     $('input').val('').focus();
   });
 
   $("#input").keyup(function(event){
-
-    if(event.keyCode == 13 && submitted === false){
+    if(event.keyCode == 13 && $('input').val() !== ''){
       $("#submit").click();
     }
-
-    //reset//
-    click = false;
   }); 
 });
+
+
+ 
--- roman-numerals/romanize.js
+++ roman-numerals/romanize.js
@@ -1,6 +1,4 @@
 //
-//
-//
 //Only goes up to 3300 for now
 
 (function(global){
@@ -14,14 +12,21 @@
       one: ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
       ten: ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
       hundred: ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
-      thousand: ["", "M", "MM", "MMM"],
+      thousand: ["", "M", "MM", "MMM",]
+    },
+
+    makeBig: function(num){
+      return ('<bigNum>' + num + '</bigNum>')
     },
 
     convert: function(num){
       this.out.unshift(this.numeral.one[parseInt(num%10/1)]);
       this.out.unshift(this.numeral.ten[parseInt(num%100/10)]);
       this.out.unshift(this.numeral.hundred[parseInt(num%1000/100)]);
-      this.out.unshift(this.numeral.thousand[parseInt(num%10000/1000)]);
+      this.out.unshift(this.makeBig(this.numeral.one[parseInt(num%10000/1000)]));
+      this.out.unshift(this.makeBig(this.numeral.ten[parseInt(num%100000/10000)]));
+      this.out.unshift(this.makeBig(this.numeral.hundred[parseInt(num%1000000/100000)]));
+      this.out.unshift(this.makeBig(this.numeral.thousand[parseInt(num%10000000/1000000)]));
       this.out = this.out.join("");
     },
   };
