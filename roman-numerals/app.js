c8ede88993e37999331501df59f7a247037694b0
Jon <jyorg@outlook.com>
2016-03-16 17:48:41 +0800

    Add error check, and effects

--- roman-numerals/app.js
+++ roman-numerals/app.js
@@ -1,16 +1,30 @@
 //
 //
 $( document ).ready(function() {  
-
+  
+  //throw error it's not a num, or if num > 1.3 million
+  function checkError(n){
+    if ( !(n).match(/\d+/) || (n > 1300000) )  {
+  	  console.log('error, not number!')
+  	  $('input').val('').focus();
+  	  $('#input').effect('shake');
+  	  throw 'not number'
+  	};   
+  }
+  
   $("#submit").on( "click", function() {
   	var input = $('input').val(), 
-  	    roman = R$(input);
+  	    roman = R(input);
+  	checkError(input);
     $("ul").prepend('<li>' + roman +'<num>' + input + '</num>' + '</li>');
+    $("ul li:first").effect("highlight", {color: '#C7F464'}, 1000);
+
     $('input').val('').focus();
   });
-
+  
   $("#input").keyup(function(event){
     if(event.keyCode == 13 && $('input').val() !== ''){
+      checkError($('input').val());
       $("#submit").click();
     }
   }); 
--- roman-numerals/romanize.js
+++ roman-numerals/romanize.js
@@ -1,7 +1,8 @@
- (function(global){
+(function(global){
+
   var Romanize = function (n){
-    var k = new Romanize.init(n);
-    return k.out
+    var roman = new Romanize.init(n);
+    return roman.out
   }
 
   Romanize.prototype = { 
@@ -11,13 +12,14 @@
       hundred: ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
       thousand: ["", "M", "MM", "MMM",]
     },
-    
+
     //Multiplies the roman numeral by x1000, by adding a border on top//
     makeBig: function(num){
       return ('<bigNum>' + num + '</bigNum>')
     },
 
     convert: function(num){
+      console.log("converting number...")
       this.out.unshift(this.numeral.one[parseInt(num%10/1)]);
       this.out.unshift(this.numeral.ten[parseInt(num%100/10)]);
       this.out.unshift(this.numeral.hundred[parseInt(num%1000/100)]);
@@ -30,6 +32,7 @@
       //million//
       this.out.unshift(this.makeBig(this.numeral.thousand[parseInt(num%10000000/1000000)]));
       this.out = this.out.join("");
+      console.log("conversion complete")
     },
   };
   
@@ -41,8 +44,8 @@
   //Set the function prototype property to the Romanize prototype, 
   Romanize.init.prototype = Romanize.prototype;
 
-  //Call R$ from the window object
-  global.R$ = Romanize;
+  //Call R from the window object
+  global.R = Romanize;
 
 }(window));
 
