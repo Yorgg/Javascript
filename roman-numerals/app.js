dad93c5125fd9d41fc23dc8c44c76c3207a80307
Jon <jyorg@outlook.com>
2016-03-16 08:11:10 +0800

    Roman numerals - Fix multiple submit error

--- roman-numerals/app.js
+++ roman-numerals/app.js
@@ -1,17 +1,25 @@
-$( document ).ready(function() {
-  // Handler for .ready() called.
- 
+$( document ).ready(function() {  
+  /* Set submitted to false */
+  var submitted = false;
+
   $("#submit").on( "click", function() {
+
+  	/* set it to true, to avoid keyup from being called if the user 
+  	   hits enter on the submit button */
+  	submitted = true;
   	var input = $('input').val();
   	var conversion = R$(input);
     $("ul").prepend("<li>" + conversion + '<number>'+input+'</number>' +"</li>");
     $('input').val('').focus();
   });
 
-  $("input").keyup(function(event){
-    if(event.keyCode == 13){
-        $("#submit").click();
-    }
-  });
+  $("#input").keyup(function(event){
 
+    if(event.keyCode == 13 && submitted === false){
+      $("#submit").click();
+    }
+
+    //reset//
+    click = false;
+  }); 
 });
